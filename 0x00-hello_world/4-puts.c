db1fa9e93a242095fe8e4c5d98ef47a5a5dc14b3
Christian Laboy <2710@holbertonschool.com>
2021-02-04 14:06:12 +0000

    part 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
  *main-prints a quote
  *Return:Ends the program
  */
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
