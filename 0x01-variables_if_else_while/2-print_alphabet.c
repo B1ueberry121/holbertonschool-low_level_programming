379803447d9a560313c2836ce47a7a17a019a6a9
Christian Laboy <2710@holbertonschool.com>
2021-02-04 18:47:03 +0000

    part 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-generates a random number, then prints a string
+ *Return: Ends the program
+ */
+
+int main(void)
+{
+	char ch = 97;
+
+	while (ch != 123)
+	{
+		while (ch != 113)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
