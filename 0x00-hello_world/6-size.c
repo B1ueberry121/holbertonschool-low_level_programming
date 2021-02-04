22c2189952c3465884853bbbd29ae6f5999162c9
Christian Laboy <2710@holbertonschool.com>
2021-02-04 15:20:49 +0000

    part 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-Starts the program
+ *Return:Ends the program
+ */
+
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int));
+	printf("%lu", sizeof(float));
+	return (0);
+}
+
