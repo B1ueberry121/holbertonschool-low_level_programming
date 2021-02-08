34e9ff1369e758db4ef705fda25416580e9312cc
Christian Laboy <2710@holbertonschool.com>
2021-02-08 18:54:45 +0000

    part 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
