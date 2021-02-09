83fb8753f3780b38476de5cde1a88301c4313922
Christian Laboy <2710@holbertonschool.com>
2021-02-09 17:32:11 +0000

    part 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Checks for caps letters
+ * c - variable
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65) && (c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
