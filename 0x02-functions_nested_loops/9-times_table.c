32948ef58d2c3e7fef72bcbfdc6962ddf794ce2a
Christian Laboy <2710@holbertonschool.com>
2021-02-10 18:06:21 +0000

    part 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * times_table - print a 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int r;
+	int r1;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			r1 = ((r / 10) + '0');
+
+			if (r1 != '0')
+			{
+				_putchar(r1);
+			}
+			else if (j > 0)
+			{
+				_putchar(' ');
+			}
+			_putchar((r % 10) + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
