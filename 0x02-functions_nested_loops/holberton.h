eca86dfd34c85584524333e37f170e1bf3b89444
Christian Laboy <2710@holbertonschool.com>
2021-02-10 00:45:04 +0000

    yeet

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,6 +4,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int);
+int print_sign(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
