6c881b473c9a961f4808ca072b1415f4a7bb62cd
Christian Laboy <2710@holbertonschool.com>
2021-02-10 14:48:41 +0000

    part 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - Shows the absolute value of an interger
+ * @i: Variable
+ * main - Shows the absolute value of an interger
+ * Return: Ends the program
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	{
+	else
+		return (*-1);
+	}
+}
