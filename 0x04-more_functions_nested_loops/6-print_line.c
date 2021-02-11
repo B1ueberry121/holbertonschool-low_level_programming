d9692dd56a5d357be612c352f106828129bec8fd
Christian Laboy <2710@holbertonschool.com>
2021-02-11 23:57:16 +0000

    part 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_line - Draws a line
+ * @n: Variable
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
