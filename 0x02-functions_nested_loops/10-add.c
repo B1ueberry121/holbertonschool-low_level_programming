b6601291dd50c19b92b00b6e88cb4e344f3dfb8e
Christian Laboy <2710@holbertonschool.com>
2021-02-11 00:14:39 +0000

    part 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers together
+ * @i: Variable
+ * @n: Variable
+ * Return: Ends the program
+ */
+
+int add(int i, int n)
+{
+	return (i + n);
+}
