4f99b1bf711663d53f92c737bccc7c13ad1f4932
Christian Laboy <2710@holbertonschool.com>
2021-02-18 14:48:58 +0000

    part 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * re_string - Reverses a string
+ * @s: pointer variable
+ */
+
+void rev_string(char *s)
+{
+	int lg = 0;
+	int sw = 0;
+	int tmp;
+
+	while (s[lg] != '\0')
+	{
+		lg++;
+	}
+	lg--;
+
+	while (sw < lg)
+	{
+		tmp = s[lg];
+		s[lg] = s[sw];
+		s[sw] = tmp;
+
+		sw++;
+		lg--;
+	}
+}
