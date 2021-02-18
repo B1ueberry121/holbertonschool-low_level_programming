05adbb9114bd8bf5515d3169ca8daa75c4c1f7c3
Christian Laboy <2710@holbertonschool.com>
2021-02-18 15:42:44 +0000

    part 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: pointer variable
+ */
+
+void puts_half(char *str)
+{
+	int i, len = 0, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	if (len % 2 != 0)
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
