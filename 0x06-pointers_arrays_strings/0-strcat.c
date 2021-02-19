9cb9eb1756b875e54a4c2599ba206a2ce6bba177
Christian Laboy <2710@holbertonschool.com>
2021-02-19 19:42:24 +0000

    New

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - Concatenates two strings
+ * @dest: pointer variable
+ * @src: pointer variable
+ */
+
+char *_strcat(char *dest, char  *src)
+{
+	int c = 0;
+	int n = 0;
+
+	while (dest[c] != '\0')
+	{
+		c++;
+	}
+	while (src[n] != '\0')
+	{
+		dest[c] = src[n];
+		n++;
+		c++;
+	}
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compares to string
+ * @s1: pointer variable
+ * @s2: pointer variable
+ */
+
+int _strcmp(char *s1, char*s2)
+{
