7d7f3933049d3cb761e0497d5ad8a4adc8b0d983
Christian Laboy <2710@holbertonschool.com>
2021-02-23 14:14:14 +0000

    part 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a stirng
+ * @s: string to be searched through
+ * @c: character to be located
+ * Return: pointer to the first ocurrence c, NULL if character is not found
+ */
+
+char *_strchr(char *s, char *c)
+{
+	int incre:
+
+	for (incre = 0; s[incre] != '\0'; incre++)
+	{
+		if (s[incre] == c)
+		{
+			return (s + incre);
+		}
+	}
+	if (s[incre] == c)
+	{
+		return (s + incre);
+	}
+	return ('\0');
+}
