4d1b213b9c9cf0d3192b33fa5b37f66497df0dc1
Christian Laboy <2710@holbertonschool.com>
2021-02-25 14:49:36 +0000

    part 5 fixing an if statement

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * sqrt_check - checks the natural square root of a number
+ * @n: root
+ * @c: number
+ * Return: the result
+ */
+
+int sqrt_check(int n, int c)
+{
+	if (n == c * c)
+	{
+		return (c);
+	}
+	else if (c >= (n / 2))
+	{
+		return (-1);
+	}
+	else
+	{
+	return (sqrt_check(n, c + 1));
+	}
+	return (0);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: root
+ * Return: the result
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		return (sqrt_check(n, 1));
+	}
+	return (0);
+}
