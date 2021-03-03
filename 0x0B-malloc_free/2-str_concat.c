b675be1ced76d0ad6971022b25be3d54e67b578c
Christian Laboy <2710@holbertonschool.com>
2021-03-03 20:15:50 +0000

    adding a new statement

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,13 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, ct = 0, rs, nrs, nsize;
 	char *nstr;
 
-	if (s1 == 0 || s2 == 0)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 	while (s1[i] != '\0')
 	{
