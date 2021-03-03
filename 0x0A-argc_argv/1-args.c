4d568665970de53ebcfd9ab9a66b3eaae2f865d3
Christian Laboy <2710@holbertonschool.com>
2021-03-03 14:00:40 +0000

    part 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it 
+ * @argc: is the counter
+ * @argv: void
+ * Return: the counter
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (argc);
+}
