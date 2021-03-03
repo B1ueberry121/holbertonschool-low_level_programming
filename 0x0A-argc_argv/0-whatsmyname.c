e438bc135e401091192970c3094068e03be4259e
Christian Laboy <2710@holbertonschool.com>
2021-03-03 13:52:49 +0000

    New

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: Argument Counter
+ * @argv: Argument Vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
