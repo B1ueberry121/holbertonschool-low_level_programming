3a57c1ae9f301aeb6de20f14580ee57e641daca3
B1ueberry121 <2710@holbertonschool.com>
2021-03-05 14:40:37 +0000

    part 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,65 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *
+ */
+
+int check_length(char *s1)
+{
+	unsigned int len = 0;
+
+	while (s1[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: number of elements
+ * Return: The unification of both strings
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len = 0, len2 = 0, nrs, rs;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	len = check_length(s1);
+	len2 = check_length(s2);
+
+	if (n >= len2)
+	{
+		n = len2;
+	}
+	ptr = malloc(len + n + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (rs = 0; rs < len; rs++)
+	{
+		ptr[rs] = s1[rs];
+	}
+	for (nrs = 0; nrs < n; nrs++)
+	{
+		ptr[rs] = s2[nrs];
+		rs++;
+	}
+	ptr[nrs + rs] = '\0';
+	return (ptr);
+}
