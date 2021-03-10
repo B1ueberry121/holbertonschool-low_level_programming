11833ae2d82794e443dc8814b0fb3f6b0942f723
B1ueberry121 <2710@holbertonschool.com>
2021-03-10 02:39:27 +0000

    part 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,10 @@ int str_length(char *s)
 
 /**
  * *new_dog - creates a new dog
- * @name:
- * @age:
- * @owner:
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: the allocated memory
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -30,7 +31,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *lemon;
 	int len = 0, len2 = 0, x;
 
-	lemon = (dog_t*) malloc(sizeof(dog_t));
+	lemon = (dog_t *) malloc(sizeof(dog_t));
 
 	if (lemon == NULL)
 	{
@@ -40,27 +41,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len2 = str_length(owner);
 
 	lemon->name = malloc(sizeof(char) * len + 1);
-	lemon->owner = malloc(sizeof(char) * len2 + 1);
 
 	if (lemon->name == NULL)
 	{
+		free(lemon);
 		return (NULL);
 	}
+	lemon->owner = malloc(sizeof(char) * len2 + 1);
+
 	if (lemon->owner == NULL)
 	{
-		return(NULL);
+		free(lemon);
+		free(lemon->name);
+		return (NULL);
 	}
-	for (x = 0; name != '\0'; x++)
+	for (x = 0; name[x] != '\0'; x++)
 	{
 		lemon->name[x] = name[x];
 	}
+	lemon->name[x] = '\0';
 
-	for (x = 0; owner != '\0'; x++)
+	for (x = 0; owner[x] != '\0'; x++)
 	{
 		lemon->owner[x] = owner[x];
 	}
-
+	lemon->owner[x] = '\0';
 	lemon->age = age;
-
 	return (lemon);
 }
