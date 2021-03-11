a109327ac18710fbf0c9fd666c5accc60a2b697c
B1ueberry121 <2710@holbertonschool.com>
2021-03-11 19:23:35 +0000

    fixing betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - sums all parameters
  * @n: hold how many parameters are coming
- * @Return: the result of the addition
+ * Return: the result of the addition
  */
 
 int sum_them_all(const unsigned int  n, ...)
@@ -13,9 +13,9 @@ int sum_them_all(const unsigned int  n, ...)
 	int y = 0;
 
 	if (n == 0)
-		return(0);
+		return (0);
 	va_start(pip, n);
-	
+
 	for (x = 0; x < n; x++)
 	{
 		y += va_arg(pip, int);
