7cfaeed124aabd21196dc352877fc3f34fa24e95
B1ueberry121 <2710@holbertonschool.com>
2021-03-11 20:27:55 +0000

    part 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints a string
+ * @separator: is the string which separates with comma and space
+ * @n: the number of parameters that will come
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list pip;
+	unsigned int x;
+	char *y;
+
+	va_start(pip, n);
+
+	for (x = 0; x < n; x++)
+	{
+		y = va_arg(pip, char *);
+		if (y == NULL)
+			printf("nil");
+		else
+			printf("%s", y);
+		if (separator != NULL && x != n - 1)
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(pip);
+}
