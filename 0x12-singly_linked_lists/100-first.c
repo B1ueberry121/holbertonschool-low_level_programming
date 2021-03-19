c00f739653a880b8e26a17af9e9871bc6640a468
B1ueberry121 <2710@holbertonschool.com>
2021-03-19 13:47:18 +0000

    first adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_inb4 - prints before the main function is executed
+ */
+
+void print_inb4(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
