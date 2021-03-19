4d14b731bfdd4699310691be887dea45eaaf2a54
B1ueberry121 <2710@holbertonschool.com>
2021-03-19 13:55:56 +0000

    changing names

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * print_inb4 - prints before the main function is executed
+ * print_in_before - prints before the main function is executed
  */
 
-void print_inb4(void)
+void print_in_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
