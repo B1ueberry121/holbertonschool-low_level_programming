d50f9b8f2fbe2aede6d9696525beacbea5bc5db3
B1ueberry121 <2710@holbertonschool.com>
2021-03-22 18:22:12 +0000

    adding for 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,9 +15,17 @@ size_t print_listint(const listint_t *h)
 	{
 		return (0);
 	}
+	
 	while (h != NULL)
 	{
+		if (h->n == 0)
+		{
+			printf("0\n");
+		}
+		else
+		{
 		printf("%i\n", h->n);
+		}
 		h = h->next;
 		count++;
 	}
