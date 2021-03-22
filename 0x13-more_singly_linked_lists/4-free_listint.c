e049f11c97444e512dacf4e702f398a5187d776d
B1ueberry121 <2710@holbertonschool.com>
2021-03-22 23:30:42 +0000

    fixing betty

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,13 +2,13 @@
 
 /**
  * free_listint - frees allocated memory
- * head: head of the link list
+ * @head: head of the link list
  */
 
 void free_listint(listint_t *head)
 {
 	listint_t *node;
-	
+
 	while (head != NULL)
 	{
 		node = head;
