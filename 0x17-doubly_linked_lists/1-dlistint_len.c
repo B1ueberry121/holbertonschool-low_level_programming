b4c4f82af5b17d2aff4c8b3adb592d11967d3605
B1ueberry121 <2710@holbertonschool.com>
2021-05-02 01:43:29 +0000

    fixing order

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,7 +8,7 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	int counter;
+	int counter = 0;
 
 	if (h == NULL)
 	{
@@ -16,8 +16,8 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 	while (h != NULL)
 	{
-		h = h->next;
 		counter++;
+		h = h->next;
 	}
 	return (counter);
 }
