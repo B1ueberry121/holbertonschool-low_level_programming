1447b8edc2de6e579fdc67a5719063c3e9be804e
B1ueberry121 <2710@holbertonschool.com>
2021-05-02 01:33:04 +0000

    New

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements
+ * @h: header of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int counter = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
