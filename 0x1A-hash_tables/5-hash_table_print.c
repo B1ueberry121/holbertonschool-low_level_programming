02edeca005b70aacafea0c0e7a8e54897669c7e4
B1ueberry121 <2710@holbertonschool.com>
2021-05-29 02:45:28 +0000

    part 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: is the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	size_t idx = 0;
+	int flag = 0;
+	hash_node_t *change_node;
+
+	if (ht)
+	{
+		printf("{");
+		while (idx < ht->size)
+		{
+			change_node = ht->array[idx];
+			while (change_node)
+			{
+				if (flag == 0)
+				{
+					printf("\'%s\': \'%s\'", change_node->key, change_node->value);
+					flag = 1;
+				}
+				else
+					printf(", \'%s\': \'%s\'", change_node->key, change_node->value);
+				change_node = change_node->next;
+			}
+			idx++;
+		}
+		printf("}\n");
+	}
+}
